f1af2485b1169e44c0471fd4b6003a44b1b769a2
htu <hturamoar@gmail.com>
2021-12-07 00:58:27 +0530

    updating application.properties

--- src/main/java/com/htu/hturecipe/repositories/CategoryRepository.java
+++ src/main/java/com/htu/hturecipe/repositories/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.htu.hturecipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.htu.hturecipe.domains.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-
+	Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/htu/hturecipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/htu/hturecipe/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,12 @@
 package com.htu.hturecipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.htu.hturecipe.domains.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
-
+	Optional<UnitOfMeasure> findByDescription(String description);
+	
 }
